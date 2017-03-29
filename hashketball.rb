6281903ee6f86f2d9614982089c8146232ff2ac2
Eric Farber <eric.j.farber@gmail.com>
2017-03-29 04:06:46 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -15,7 +15,7 @@ def game_hash
                     :steals => 3,
                     :blocks => 1,
                     :slam_dunks => 1
-                }
+                },
                 :"Reggie Evans" => {
                     :number => 30,
                     :shoe => 14,
@@ -25,7 +25,7 @@ def game_hash
                     :steals => 12,
                     :blocks => 12,
                     :slam_dunks => 7
-                }
+                },
                 :"Brook Lopez" => {
                     :number => 11,
                     :shoe => 17,
@@ -35,7 +35,7 @@ def game_hash
                     :steals => 3,
                     :blocks => 1,
                     :slam_dunks => 15
-                }
+                },
                 :"Mason Plumlee" => {
                     :number => 1,
                     :shoe => 19,
@@ -45,7 +45,7 @@ def game_hash
                     :steals => 3,
                     :blocks => 8,
                     :slam_dunks => 5
-                }
+                },
                 :"Jason Terry" => {
                     :number => 31,
                     :shoe => 15,
@@ -62,18 +62,98 @@ def game_hash
             :team_name => "Charlotte Hornets",
             :colors => ["Turquoise", "Purple"],
             :players => {
-                :name => {
-                    :number => "",
-                    :shoe => "",
-                    :points => "",
-                    :rebounds => "",
-                    :assists => "",
-                    :steals => "",
-                    :blocks => "",
-                    :slam_dunks => ""
-                }
+                :"Jeff Adrien" => {
+                    :number => 4,
+                    :shoe => 18,
+                    :points => 10,
+                    :rebounds => 1,
+                    :assists => 1,
+                    :steals => 2,
+                    :blocks => 7,
+                    :slam_dunks => 2
+                },
+                :"Bismak Biyombo" => {
+                    :number => 0,
+                    :shoe => 16,
+                    :points => 12,
+                    :rebounds => 4,
+                    :assists => 7,
+                    :steals => 7,
+                    :blocks => 15,
+                    :slam_dunks => 10
+                },
+                :"DeSagna Diop" => {
+                    :number => 2,
+                    :shoe => 14,
+                    :points => 24,
+                    :rebounds => 12,
+                    :assists => 12,
+                    :steals => 4,
+                    :blocks => 5,
+                    :slam_dunks => 5
+                },
+                :"Ben Gordon" => {
+                    :number => 8,
+                    :shoe => 15,
+                    :points => 33,
+                    :rebounds => 3,
+                    :assists => 2,
+                    :steals => 1,
+                    :blocks => 1,
+                    :slam_dunks => 0
+                },
+                :"Brendan Haywood" => {
+                    :number => 33,
+                    :shoe => 15,
+                    :points => 6,
+                    :rebounds => 12,
+                    :assists => 12,
+                    :steals => 22,
+                    :blocks => 5,
+                    :slam_dunks => 12
+                },
             }
         }
     }
 
+    game
+
 end
+
+def home_team_name
+    game_hash[:home][:team_name]
+end
+
+def good_practices
+    game_hash.each do |location, team_data|
+        team_data.each do |attribute, data|
+
+        end
+    end
+end
+
+def num_points_scored(name)
+    # binding.pry
+    if game_hash[:home][:players].keys().include? name.to_sym
+        game_hash[:home][:players][name.to_sym][:points]
+    elsif game_hash[:away][:players].keys().include? name.to_sym
+        game_hash[:away][:players][name.to_sym][:points]
+    end
+end
+
+def shoe_size(name)
+
+    if game_hash[:home][:players].keys().include? name.to_sym
+        game_hash[:home][:players][name.to_sym][:shoe]
+    elsif game_hash[:away][:players].keys().include? name.to_sym
+        game_hash[:away][:players][name.to_sym][:shoe]
+    end
+end
+
+
+def team_colors(team_name)
+    binding.pry
+
+end
+
+team_colors("Brooklyn Nets")
